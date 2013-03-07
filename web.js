bdb75fd4c43bee6bff79f413fc9c57e222dbbdb8
draeton <draeton@gmail.com>
2013-03-07 02:13:22 -0500

    Reverting back to app code after debug

--- web.js
+++ web.js
@@ -4,20 +4,7 @@
 
 "use strict";
 
-//var perfreqs = require("./lib/perfreqs");
-//var pr = perfreqs();
+var perfreqs = require("./lib/perfreqs");
+var pr = perfreqs();
 
-//pr.start();
-
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+pr.start();
