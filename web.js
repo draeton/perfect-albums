08da1005dec068b45158d158db23e7d47622165b
draeton <draeton@gmail.com>
2013-03-07 02:07:32 -0500

    testing for heroku

--- web.js
+++ web.js
@@ -4,7 +4,20 @@
 
 "use strict";
 
-var perfreqs = require("./lib/perfreqs");
-var pr = perfreqs();
+//var perfreqs = require("./lib/perfreqs");
+//var pr = perfreqs();
 
-pr.start();
+//pr.start();
+
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
